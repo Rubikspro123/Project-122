bc22957eb996451dd9c02e02714183fbd131e0e9
Rubikspro123 <87181104+Rubikspro123@users.noreply.github.com>
2022-12-22 15:55:00 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,18 +4,17 @@ screen_width = 0;
 screen_height = 0;
 apple = '';
 speak_data = '';
-to_number = '';
+to_number = 0;
 content = '';
-
 draw_apple = "";
 
+function preload(){
+  apple = loadImage('apple.png')
+}
 var SpeechRecognition = window.webkitSpeechRecognition;
   
 var recognition = new SpeechRecognition();
 
-function preload(){
-  loadImage('apple.png')
-}
 function start()
 {
   document.getElementById("status").innerHTML = "System is listening please speak";  
@@ -25,6 +24,8 @@ function start()
 recognition.onresult = function(event) {
 
  console.log(event); 
+ content = event.results[0][0].transcript;
+ document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
  to_number = Number(content);
  if(Number.isInteger(to_number)){
   document.getElementById('status').innerHTML = 'Started drawing apple';
@@ -33,11 +34,6 @@ recognition.onresult = function(event) {
  else{
   document.getElementById('status').innerHTML = 'The speech has not recognized the number';
  }
-
- content = event.results[0][0].transcript;
-
-    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
-
 }
 
 function setup() {
@@ -50,14 +46,15 @@ function setup() {
 function draw() {
   if(draw_apple == "set")
   {
-    document.getElementById("status").innerHTML = to_number + " Apples drawn";
-    draw_apple = "";
     for(var i = 1; i <= to_number; i++){
       x = Math.floor(Math.random() * 700);
       y = Math.floor(Math.random() * 400);
       image(apple, x, y, 50, 50);
     }
-    speak_data = to_number + 'Apple is Drawn'
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    speak_data = to_number + 'Apple is Drawn';
+    speak();
+    draw_apple = "";
   }
 }
 
